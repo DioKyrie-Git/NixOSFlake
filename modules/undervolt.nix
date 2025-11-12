35fdcccf9c821b9b5869d74af1d1a5363c82e789
Dio Kyrie <DioKyrie@proton.me>
2025-11-12 02:01:18 +0200

    update undervolt

--- modules/undervolt.nix
+++ modules/undervolt.nix
@@ -2,6 +2,20 @@
   # Undervolt Intel CPU & GPU
   services.undervolt = {
     enable = true;
+    turbo = 1;
+    coreOffset = -50;
+    gpuOffset = -50;
     uncoreOffset = -50;
+    analogioOffset = -50;
+    tempAc = 70;
+    tempBat = 50;
+    p1 = {
+      limit = 12;
+      window = 28;
+    };
+    p2 = {
+      limit = 35;
+      window = 0.00244;
+    };
   };
 }
